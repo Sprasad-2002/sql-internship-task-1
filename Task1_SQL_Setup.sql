f19aecf41633a4ad97a75f431fb2c3bd03c7349b
Shakti Prasad Jena <139731403+Sprasad-2002@users.noreply.github.com>
2026-01-15 17:15:17 +0530

    Add files via upload

---
+++ Task1_SQL_Setup.sql
@@ -0,0 +1,20 @@
+#CREATE DATABASE intern_training_db;
+
+#USE intern_training_db;
+
+#CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    age INT
+);
+
+#INSERT INTO students (name, email, age)
+ VALUES
+('Rahul Sharma', 'rahul@gmail.com', 22),
+('Priya Singh', 'priya@gmail.com', 21),
+('Amit Das', 'amit@gmail.com', 23),
+('Neha Patel', 'neha@gmail.com', 20),
+('Suresh Kumar', 'suresh@gmail.com', 24);
+
+select * from students;
