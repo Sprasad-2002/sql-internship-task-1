4a1b4197d3a73d3d773e8a7530c34655e901ac4d
Shakti Prasad Jena <139731403+Sprasad-2002@users.noreply.github.com>
2026-01-15 17:16:41 +0530

    Update Task1_SQL_Setup.sql

--- Task1_SQL_Setup.sql
+++ Task1_SQL_Setup.sql
@@ -1,15 +1,15 @@
-#CREATE DATABASE intern_training_db;
+CREATE DATABASE intern_training_db;
 
-#USE intern_training_db;
+USE intern_training_db;
 
-#CREATE TABLE students (
+CREATE TABLE students (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100),
     email VARCHAR(100),
     age INT
 );
 
-#INSERT INTO students (name, email, age)
+INSERT INTO students (name, email, age)
  VALUES
 ('Rahul Sharma', 'rahul@gmail.com', 22),
 ('Priya Singh', 'priya@gmail.com', 21),
